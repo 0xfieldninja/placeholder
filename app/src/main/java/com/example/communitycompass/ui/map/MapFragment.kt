78816d70e9450f33f9030ab15bf256b9f31ca59a
0xfieldninja <0xhomeless@gmail.com>
2024-07-11 13:00:21 -0700

    Update MapFragment.kt

--- app/src/main/java/com/example/communitycompass/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/communitycompass/ui/map/MapFragment.kt
@@ -1,1 +1,45 @@
+package com.example.communitycompass.ui.map
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.communitycompass.databinding.FragmentMapBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    companion object {
+        fun newInstance() = MapFragment()
+    }
+
+    private val viewModel: MapViewModel by viewModels()
+    private var _binding: FragmentMapBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var map: GoogleMap
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        // Set up map settings and add markers here
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
